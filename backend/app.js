bd5e55a1afd4f7cf011248a0484fe5bbb41b6594
Rohini P Sidder <116884424+RohiniPSidder@users.noreply.github.com>
2024-10-14 04:15:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config({path:"./config/config.env"});
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: https://event-website-api.vercel.app/,
     methods: ["POST"],
     credentials: true,
 }));
