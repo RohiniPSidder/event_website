6c6f252857bec71f74167efb02aa9557ece9e98d
Rohini P Sidder <116884424+RohiniPSidder@users.noreply.github.com>
2024-10-14 04:23:29 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "https://event-website-api.vercel.app/",
+        "http://localhost:4000/api/v1/message/send",
         {
           name,
           email,
